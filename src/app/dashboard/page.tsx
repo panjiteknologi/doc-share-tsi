0cd8c6e1eebe45a58b72d988c37774c4049f7e24
panjiteknologi <itserv.panji@gmail.com>
2025-04-26 21:31:08 +0700

    create layout

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,11 +1,10 @@
-import { AppSidebar } from "@/components/app-sidebar";
-import { SectionCards } from "@/components/section-cards";
-import { SiteHeader } from "@/components/site-header";
-import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
 import { auth } from "@/lib/auth";
+import { SectionCards } from "@/components/section-cards";
 import { redirect } from "next/navigation";
 
-export default async function Page() {
+import LayoutDashboard from "@/layout/LayoutDashboard";
+
+export default async function DashboardPage() {
   const session = await auth();
 
   if (!session) {
@@ -13,25 +12,8 @@ export default async function Page() {
   }
 
   return (
-    <SidebarProvider
-      style={
-        {
-          "--sidebar-width": "calc(var(--spacing) * 72)",
-          "--header-height": "calc(var(--spacing) * 12)",
-        } as React.CSSProperties
-      }
-    >
-      <AppSidebar variant="inset" />
-      <SidebarInset>
-        <SiteHeader />
-        <div className="flex flex-1 flex-col">
-          <div className="@container/main flex flex-1 flex-col gap-2">
-            <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
-              <SectionCards />
-            </div>
-          </div>
-        </div>
-      </SidebarInset>
-    </SidebarProvider>
+    <LayoutDashboard>
+      <SectionCards />
+    </LayoutDashboard>
   );
 }
