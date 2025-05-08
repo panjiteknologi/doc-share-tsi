0d9e90bcd8da160481ad6c9563a76018f36cb975
panjiteknologi <itserv.panji@gmail.com>
2025-05-08 10:48:17 +0700

    default to auditors

--- src/views/dashboard/dashboard-tabs.tsx
+++ src/views/dashboard/dashboard-tabs.tsx
@@ -1,6 +1,3 @@
-"use client";
-
-import { useState } from "react";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { TableClients } from "./tables/table-clients";
 import { TableAuditors } from "./tables/table-auditors";
@@ -8,13 +5,10 @@ import { TableFolders } from "./tables/table-folders";
 import { TableDocuments } from "./tables/table-documents";
 
 export function DashboardTabs() {
-  const [activeTab, setActiveTab] = useState("clients");
-
   return (
     <Tabs
-      defaultValue="clients"
+      defaultValue="auditors"
       className="flex w-full flex-col justify-start mt-10"
-      onValueChange={setActiveTab}
     >
       <TabsList>
         <TabsTrigger value="auditors">Auditors</TabsTrigger>
